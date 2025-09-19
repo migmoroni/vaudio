0a13d63df5225d084dc76a789f2d82f5def7862e
Miguel Moroni <miguelmcontato@gmail.com>
2025-09-18 22:52:13 -0300

    fix: types to dist

--- shared/types/types.js
+++
@@ -1,8 +0,0 @@
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.AppStatus = void 0;
-var AppStatus;
-((AppStatus) => {
-  AppStatus['LOADING'] = 'loading';
-  AppStatus['READY'] = 'ready';
-  AppStatus['ERROR'] = 'error';
-})(AppStatus || (exports.AppStatus = AppStatus = {}));
--- shared/types/utils.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function getSharedMessage(): string;
-export declare function formatDate(date: Date): string;
-export declare function validateEmail(email: string): boolean;
-export declare function generateId(): string;
